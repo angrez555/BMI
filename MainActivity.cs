eff24bc8b78a8e5454087e00f0e6e760a5e3182c
Angrej Singh <AS119@atcltd.atc.org.nz>
2021-05-14 08:49:04 +1200

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+
+namespace BMI
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button btnBMI;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            btnBMI = FindViewById<Button>(Resource.Id.btnBMI);
+            btnBMI.Click += onBtnBMIClick;
+
+        }
+
+        private void onBtnBMIClick(object sender, EventArgs e)
+        {
+            StartActivity(typeof(BMI));
+        }
+
+        //private void onBtnBMIClick(object sender, EventArgs e)
+        //{
+        //    StartActivity(typeof(BMI));
+        //}
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
